7752d4d91be0dcbabbee4534879f3ef8407b5054
Jan Tojnar <jtojnar@gmail.com>
2020-12-10 07:53:43 +0100

    10b: Add recursive solution
    It is based on dynamic programming but we still need to build the table.

--- 10/solution.hs
+++ 10/solution.hs
@@ -4,9 +4,16 @@ count :: Bool -> Int
 count True = 1
 count False = 0
 
+valid :: [Int] -> Int -> Int
+valid adapters 0 = 1
+valid adapters n = if n `elem` adapters then valid adapters (n-1) + valid adapters (n-2) + valid adapters (n-3) else 0
+
 main :: IO ()
 main = do
     adapters <- sort . map read . lines <$> readFile "input"
-    let differences = zipWith (-) adapters (0:adapters)
-    let (threes, ones) = foldr (\n (threes', ones') -> (threes' + count (n == 3), ones' + count (n == 1))) (1, 0) differences
+    let device = maximum adapters + 3
+    let adapters' = adapters ++ [device]
+    let differences = zipWith (-) adapters' (0:adapters')
+    let (threes, ones) = foldr (\n (threes', ones') -> (threes' + count (n == 3), ones' + count (n == 1))) (0, 0) differences
     print (threes * ones)
+    print (valid adapters' device)
